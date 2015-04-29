f72fd0b76cc5b68d8127251fb7917fa732e98c25
nicolas biencourt <nicolasbiencourt@me.com>
2015-04-29 15:39:17 +0200

    Details

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,40 @@
 class BookingsController < ApplicationController
 
+  def index
+
+  end
+
+  def new
+    @task = Task.find(params[:task_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @task = Task.find(params[:task_id])
+    @booking = @task.bookings.new(booking_params)
+    if @booking.save
+      @booking.update(user_id: current_user.id)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update_attributes(update_booking_params)
+    redirect_to :back
+  end
+
+  private
+
+  def update_booking_params
+    params.require(:booking).permit(:accepted)
+  end
+
+  def booking_params
+    params.require(:booking).permit(:date)
+  end
 end
+
 
