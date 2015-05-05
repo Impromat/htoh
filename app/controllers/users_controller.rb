6d9243517a00ea0949f5e493c64f6498334a290a
mathieu lhoste <lhoste.mathieu@gmail.com>
2015-05-05 17:01:45 +0200

    ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,4 +15,14 @@ class UsersController < ApplicationController
     # @answers = Booking.where(task_id: @tasks_ids)
   end
 
+  def update
+    current_user = User.find(:id)
+    current_user.update(user_params)
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :picture, :email, :password)
+    end
 end
